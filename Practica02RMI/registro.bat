647ffedae9b952fb297fac1ea9facb236b0e71ff
Miguel Ángel León Bardavío <miguelelleon@gmail.com>
2018-04-12 11:51:31 +0200

    Arreglo del servidor

--- Practica02RMI/registro.bat
+++ Practica02RMI/registro.bat
@@ -1,3 +1,3 @@
-rem set CLASSPATH=.\target\Practica2RMI-0.0.1-SNAPSHOT.jar
+rem set CLASSPATH=.\target\Practica02RMI-0.0.1-SNAPSHOT.jar
 
-rmiregistry -J-Djava.rmi.server.codebase=http://localhost:8080/Practica2RMI-Web/
+rmiregistry -J-Djava.rmi.server.codebase=http://localhost:8080/Practica02RMI-Web/
